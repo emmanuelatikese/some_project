d4dc03ef10fb6561e5a5f3f9d2263e7a74f4ee77
emmanuelatikese <emmanuelatikese14@gmail.com>
2025-03-15 00:36:22 +0000

    new code I guess

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	other_funcs "master_algo/utils"
 	"os"
 	"strings"
 )
@@ -22,6 +24,16 @@ func init(){
 	// checking for mp3
 	if strings.HasSuffix(filename, ".mp3"){
 			// if mp3 convert to wav and remain the file name
+			filename, err := other_funcs.ConvertMP3ToWav(filename);
+			if err != nil {
+				log.Println(err);
+				return;
+			}
+
+			_,err = os.Open(filename)
+			if !(os.IsExist(err))  {
+				fmt.Println("The filename:", filename, "has been converted to wav file.")
+			}
 	}
 
 
